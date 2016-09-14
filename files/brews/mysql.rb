5305c4101876593830b0b70b3d35d4ebeb321690
Lucas Wilson-Richter <lucas.wilsonrichter@redbubble.com>
2016-09-14 12:02:27 +1000

    Update brew download URL
    The CDN only serves the last few patch releases. This URL will let us access this version long term.

--- files/brews/mysql.rb
+++ files/brews/mysql.rb
@@ -1,7 +1,7 @@
 class Mysql < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.6/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.31.tar.gz"
+  url "https://dev.mysql.com/Downloads/MySQL-5.6/mysql-5.6.31.tar.gz"
   sha256 "6df1389bbf899025aee6be0f4a12b8b0135e6de7db83e3ea20201ad3633ba424"
 
   option :universal
